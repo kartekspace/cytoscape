18ed50985b809bb29cd9fadad37f0e2c9816dedd
kartekspace <karthikeyanvijayaraja@gmail.com>
2021-11-23 10:27:10 +0530

    2 files modified

--- index.js
+++ index.js
@@ -12,15 +12,24 @@ var cy = cytoscape({
   style: cytoscape.stylesheet()
     .selector('node')
       .style({
-        'content': 'data(id)'
+        'content': function( ele ){ 
+          return ele.data('heading') + '\n' + ele.data('user') + '\n' +
+          ele.data('attribute'); },
+        'shape': 'rectangle',
+        'width': '300',
+        'height': '200',
+        'text-wrap': 'wrap',
+        'text-halign': 'center',
+        'text-valign': 'center',
+        'font-size':'30',        
       })
     .selector('edge')
       .style({
         'curve-style': 'bezier',
         'target-arrow-shape': 'triangle',
-        'width': 4,
+        'width': 12,
         'line-color': '#ddd',
-        'target-arrow-color': '#ddd'
+        'target-arrow-color': 'black'
       })
     .selector('.highlighted')
       .style({
@@ -33,15 +42,45 @@ var cy = cytoscape({
 
   elements: {
       nodes: [
-        { data: { id: 'a' } },
-        { data: { id: 'b' } },
-        { data: { id: 'c' } },
-        { data: { id: 'd' } },
-        { data: { id: 'e' } },
-        { data: { id: 'f' } },
-        { data: { id: 'g' } },
-        { data: { id: 'h' } },
-        { data: { id: 'i' } }
+        { data: {
+           id: 'a',
+        heading:'Access Cluster',
+        user:'bob',
+        attribute:'access project'
+       } },
+        { data: { id: 'b',
+        heading:'Account 01',
+        user:'bob',
+        attribute:'access project'
+       } },
+        { data: { id: 'c',
+        heading:'Group of networks 01',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'd',
+        heading:'Network 01',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'e',
+        heading:'Network 02',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'f',
+        heading:'Group of networks 02',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'g',
+        heading:'Network 03',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'h',
+        heading:'Network 04',
+        user:'bob',
+        attribute:'access project' } },
+        { data: { id: 'i',
+        heading:'Account 02',
+        user:'bob',
+        attribute:'access project' } }
       ],
 
       edges: [
@@ -76,6 +115,8 @@ var highlightNextEle = function(){
     setTimeout(highlightNextEle, 1000);
   }
 };
-
+window.addEventListener('resize', function(event){
+  cy.center();
+});
 // kick off first highlight
-highlightNextEle();
+//highlightNextEle();
